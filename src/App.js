dc27026bd88011e33a23b981c57ad7d46cc7cc4f
mimi klein <lifeistoogoodtobetrue@gmail.com>
2018-01-14 14:36:46 -0500

    add api call onsubmit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      results: []
+    }
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    let query = event.target.search_text.value;
+    axios.get(`https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch=${query}`)
+      .then(response => {
+        let results = response.data.query.search;
+        console.log(response.data.query.search);
+        this.setState({ results: results })
+      })
+    event.target.search_text.value = "";
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Special:Random"><button className="random">Click here for random knowledge</button></a>
+        <form className="form" onSubmit={this.onSubmit}>
+          <input className="input" ype="text" name="search_text" />
+          <button type="submit">Search</button>
+        </form>
+        <div>
+          {this.state.results ? this.state.results.map((article, i) => {
+            return (
+              <a href={`https://en.wikipedia.org/?curid=${article.pageid}`}
+                target="_blank"
+                rel="noopener noreferrer">
+                <div className="result-list" key={i}>
+                  <p className="title">{article.title}</p>
+                  <p className="snippet" dangerouslySetInnerHTML={{ __html: `${article.snippet} ...(Click for more)` }} />
+                </div>
+              </a>
+            )
+          }) : null}
+        </div>
       </div>
     );
+
   }
 }
 
